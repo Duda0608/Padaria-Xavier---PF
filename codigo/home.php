5a8cbd0d1bb286d7be01ac3570368d137d13863d
Nalaura2002 <anaalmeidaaam@gmail.com>
2025-06-12 15:24:06 -0300

    fiz home

--- codigo/home.php
+++ codigo/home.php
@@ -6,4 +6,19 @@
     <title>Pagina Inicial</title>
 </head>
 <body>
-    
+    <ul>
+        <li>
+        <a href="formusuario.php">Cadastrar novo usuario</a>
+        </li>
+        <li>
+            <a href="listarusuarios.php">Lista de usuario existentes</a>
+        </li>
+        <li>
+            <a href="formprodutos.php">Cadastrar produtos</a>
+        </li>
+        <li>
+            <a href="listarprodutos.php">Lista produtos existentes</a>
+        </li>
+    </ul>
+</body>    
+</html>
