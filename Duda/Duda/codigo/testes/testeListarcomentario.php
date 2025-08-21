96c61597d36443715a3ffc79dce0e0bbad1d83a7
Duda0608 <mariaedureis0608@gmail.com>
2025-08-21 13:51:12 -0300

    Exclui arquivos duplicados
    ...

--- Duda/Duda/codigo/testes/testeListarcomentario.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../conexao.php";
-require_once "../funcoes.php";
-
-echo "=== TESTE LISTAR COMENTARIO ===\n";
-echo "<pre>";
-print_r(listarcomentarios($conexao));
-echo "</pre>\n";
-?>
--- Duda/Duda/codigo/testes/teste_Deletarhistorico.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../conexao.php";
-require_once "../funcoes.php";
-
-echo "=== TESTE DELETAR HISTORICO ===\n";
-$idhistorico = deletarhistorico($conexao, $idhistorico);
-echo "Historico deletado: " . ($idhistorico ? "Sucesso" : "Falha") . "\n\n";
-
-?>
