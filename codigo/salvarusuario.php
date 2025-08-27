65d5e21fe95c2cdeb0e438cc7c885852200d9256
Cindymello <cindy.melo@estudante.ifgoiano.edu.br>
2025-08-27 10:00:26 -0300

    Update salvarusuario.php

--- codigo/salvarusuario.php
+++ codigo/salvarusuario.php
@@ -15,7 +15,14 @@ $gerenciapromo = 0;
 
 $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO tb_usuarios (nome, cpf, telefone, endereco, email, senha, controlelogin, gerenciapromo) VALUES ('$nome', '$cpf', '$telefone', '$endereco', '$email', '$senha_hash', '$controlelogin', '$gerenciapromo')";
+if ($id == 0) {
+
+    salvarusuario($conexao, $nome, $cpf, $telefone, $endereco, $email, $senha, $administrador, $controlelogin, $gerenciapromo);
+
+} else {
+    $sql = "INSERT INTO tb_usuarios (nome, cpf, telefone, endereco, email, senha, controlelogin, gerenciapromo) VALUES ('$nome', '$cpf', '$telefone', '$endereco', '$email', '$senha_hash', '$controlelogin', '$gerenciapromo')";
+
+}
 
 mysqli_query($conexao, $sql);
 
