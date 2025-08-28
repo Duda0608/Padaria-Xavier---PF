eab602c5070d691cf5abd0117b311ddcde1112af
Cindymello <cindy.melo@estudante.ifgoiano.edu.br>
2025-08-28 13:56:59 -0300

    usuário tudo certo

--- codigo/salvarusuario.php
+++ codigo/salvarusuario.php
@@ -10,21 +10,23 @@ $telefone = $_POST['telefone'];
 $endereco = $_POST['endereco'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+$administrador = 0;
 $controlelogin = 0;
 $gerenciapromo = 0;
 
 $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
+$id = isset($_POST['idusuario']) ? $_POST['idusuario'] : 0;
+
 if ($id == 0) {
 
     salvarusuario($conexao, $nome, $cpf, $telefone, $endereco, $email, $senha, $administrador, $controlelogin, $gerenciapromo);
+    header("Location: index.php");
 
 } else {
-    $sql = "INSERT INTO tb_usuarios (nome, cpf, telefone, endereco, email, senha, controlelogin, gerenciapromo) VALUES ('$nome', '$cpf', '$telefone', '$endereco', '$email', '$senha_hash', '$controlelogin', '$gerenciapromo')";
-
+    editarusuario($conexao, $nome, $cpf, $telefone, $endereco, $email, $senha, $administrador, $controlelogin, $gerenciapromo, $idusu);
 }
 
-mysqli_query($conexao, $sql);
 
-header("Location: index.php");
+header("Location: home.php");
 ?>
