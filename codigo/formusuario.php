37ec18d924bff11345f942df582809e61f82e54b
Cindymello <cindy.melo@estudante.ifgoiano.edu.br>
2025-09-15 16:49:56 -0300

    algumas alterações no formusuario

--- codigo/formusuario.php
+++ codigo/formusuario.php
@@ -39,23 +39,82 @@ if(isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="../jquery-3.7.1.min.js"></script>
+    <script src="../jquery.validate.min.js"></script>
+    <script>
+        $(document).ready(function () {
+                $("#formusuario").validate({
+                    rules: {
+                        nome: {
+                            required: true,
+                        },
+                        cpf: {
+                            required: true,
+                            minlength: 11,
+                            digits: true,
+                        },
+                        telefone: {
+                            required: true,
+                            minlength: 11,
+                            digits: true,
+                        },
+                        endereco: {
+                            required: true,
+                        },
+                        email: {
+                            required: true,
+                        },
+                        senha: {
+                            required: true,
+                        },
+
+                    messages: {
+                        nome: {
+                            required: "Esse campo deve ser preenchido",
+                        },
+                        cpf: {
+                            required: "Informe seu CPF",
+                        },
+                        telefone: {
+                            required: "Nos informe de seu telefone para entrarmos em contato",
+                        },
+                        endereco: {
+                            required: "Informe-nos de seu endereço caso seja entrega",
+                        },
+                        email: {
+                            required: "Este campo dever ser preenchido",
+                        },
+                        senha: {
+                            required: "É necessário uma senha para entrar",
+                        },
+                    }
+                }
+            })
+        });
+</script>
+<style>
+    .error {
+        color: red;
+    }
+</style>
+             
 </head>
 <body>
-    <h1>Acesso ao sistemaaaaaaa</h1>
+    <h1>Acesso ao sistema</h1>
 
-    <form action="salvarusuario.php?id=<?php echo $id; ?>" method="post">
+    <form id="formusuario" action="salvarusuario.php?id=<?php echo $id; ?>" method="post">
         Nome:<br>
-        <input type="text" name="nome" value="<?php echo $nome; ?>"><br>
+        <input type="text" name="nome" id="nome" value="<?php echo $nome; ?>"><br>
         Cpf:<br>
-        <input type="text" name="cpf" value="<?php echo $cpf; ?>"><br>
+        <input type="text" name="cpf" id="cpf" value="<?php echo $cpf; ?>"><br>
         Telefone:<br>
-        <input type="text" name="telefone" value="<?php echo $telefone; ?>"><br>
+        <input type="text" name="telefone" id="telefone" value="<?php echo $telefone; ?>"><br>
         Endereço:<br>
-        <input type="text" name="endereco" value="<?php echo $endereco; ?>"><br>
+        <input type="text" name="endereco" id="endereco" value="<?php echo $endereco; ?>"><br>
         Email:<br>
-        <input type="text" name="email" value="<?php echo $email; ?>"><br>
+        <input type="text" name="email" id="email" value="<?php echo $email; ?>"><br>
         Senha:<br>
-        <input type="password" name="senha" value="<?php echo $senha; ?>"><br><br>
+        <input type="password" name="senha" id="senha" value="<?php echo $senha; ?>"><br><br>
 
         <input type="submit" value="<?php echo $botao; ?>">
         
