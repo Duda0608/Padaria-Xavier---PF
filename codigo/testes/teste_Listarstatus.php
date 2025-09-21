ae0440a30c3854197f38f161aed2ffff349ece74
Nalaura2002 <anaalmeidaaam@gmail.com>
2025-09-21 16:49:41 -0300

    s
    s

--- codigo/testes/teste_Listarstatus.php
+++ codigo/testes/teste_Listarstatus.php
@@ -2,18 +2,7 @@
 require_once "../conexao.php";
 require_once "../funcoes.php";
 
-echo "=== TESTE LISTAR STATUS ===\n";
-
-$pedidos = listarstatus($conexao);
-
-foreach ($pedidos as $pedido) {
-    echo "ID: " . $pedido['idpedido'] . "\n";
-    echo "Data: " . $pedido['data'] . "\n";
-    echo "Valor: R$ " . number_format($pedido['valor'], 2, ',', '.') . "\n";
-    echo "Pagamento: " . $pedido['pagamento'] . "\n";
-    echo "Entrega: " . $pedido['entrega'] . "\n";
-    echo "Status: " . $pedido['status'] . "\n";
-    echo "Cliente ID: " . $pedido['tb_cliente_idcliente'] . "\n";
-    echo "--------------------------\n";
-}
-?>
+echo "<pre>";
+print_r(listarstatus($conexao));
+echo"oii";
+echo "</pre>";
