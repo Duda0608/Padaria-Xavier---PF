66367e89585530fa8c750dde04321eabb3030b47
Nalaura2002 <anaalmeidaaam@gmail.com>
2025-10-09 13:43:47 -0300

    Update formusuario.php

--- codigo/formusuario.php
+++ codigo/formusuario.php
@@ -111,31 +111,7 @@ if (isset($_GET['id'])) {
 </head>
 
 <body class="corpousuario">
-    <div class="logo"> <img src="logo.png" alt="Logo da Empresa"> </div>
-        <div class="container">
-            <div class="row h-100">
-                <div class="col-md-6 imgesquerda d-none d-md-block"></div>
-                    <div class="col-md-6 direita">
-                    <div class="formusuario">
-                        <h1>Criar Conta</h1>
-                        <p>Preencha suas informações para se cadastrar</p>
-                        <form id="formusuario" method="post" action="salvarusuario.php"> <input type="hidden" name="id" value="<?= $id ?>">
-                            <div class="mb-3"> <label for="nome">Nome:</label> <input type="text" class="campusu" id="nome" name="nome" value="<?= $nome ?>"> </div>
-                            <div class="row mb-3">
-                                <div class="col-md-6"> <label for="cpf">CPF:</label> <input type="text" class="campusu" id="cpf" name="cpf" value="<?= $cpf ?>"> </div>
-                                <div class="col-md-6"> <label for="telefone">Telefone:</label> <input type="text" class="campusu" id="telefone" name="telefone" value="<?= $telefone ?>"> </div>
-                            </div>
-                            <div class="mb-3"> <label for="email">Email:</label> <input type="email" class="campusu" id="email" name="email" value="<?= $email ?>"> </div>
-                            <div class="mb-3"> <label for="senha">Senha:</label> <input type="password" class="campusu" id="senha" name="senha" value="<?= $senha ?>"> </div> <button type="submit" class="botaousu"><?= $botao ?></button>
-                        </form>
-                        <div class="login-link"> Já tem uma conta? <a href="#">Entre aqui</a> </div>
-
-                    </div>                
-                </div>
-            </div>
-            <footer>Sistema de Acesso Seguro</footer>
-        </div>
-    </div>
+ 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../jquery-3.7.1.min.js"></script>
     <script src="../jquery.validate.min.js"></script>
@@ -160,7 +136,7 @@ if (isset($_GET['id'])) {
                 SENHA:<br>
                 <input name="senha" type="password" placeholder="informe sua senha" aria-label="Disabled input example" value="<?php echo $senha; ?>"> <br><br>
 
-                <button class="butao" type="submit" class="btn" data-bs-toggle="button"><?php echo $botao; ?></button>
+                <input type="submit" value="<?php echo $botao; ?>">
         </form>
     </div>
 
